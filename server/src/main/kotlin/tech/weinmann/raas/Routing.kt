15c4b373348bc1462bb859f2dbff40141bfbf4da
kay <kay.richter@googlemail.com>
2025-08-16 21:05:30 +0200

    wip: added some routing and dependencie injection

--- server/src/main/kotlin/tech/weinmann/raas/Routing.kt
+++ server/src/main/kotlin/tech/weinmann/raas/Routing.kt
@@ -10,11 +10,37 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.*
+import org.koin.ktor.ext.inject
+import tech.weinmann.raas.devices.DeviceRepoInterface
+import org.koin.ktor.ext.inject
+import org.koin.ktor.plugin.*
+import tech.weinmann.raas.configuration.RpiConfiguration
+
 
 fun Application.configureRouting() {
+    val deviceService by inject<DeviceRepoInterface>()
+
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        route("/api/v1"){
+            route ("/machine"){
+                get("{serial}"){
+                    val serial = call.parameters["serial"]!!
+                    val conf = deviceService.read(serial = serial)
+                    if (conf != null){
+                        call.respond(conf)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
+                }
+                post<RpiConfiguration> {
+                    deviceService.create(it)
+                }
+            }
+        }
+        // This is needed to be possible without registration!
+
     }
 }
