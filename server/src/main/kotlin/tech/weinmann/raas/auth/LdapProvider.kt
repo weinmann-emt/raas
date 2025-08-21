c0a1a0f2ff573904e4287ad1f6413768f14a0bce
knkay <kay.richter@googlemail.com>
2025-08-21 09:52:08 +0200

    Search working

--- server/src/main/kotlin/tech/weinmann/raas/auth/LdapProvider.kt
+++ server/src/main/kotlin/tech/weinmann/raas/auth/LdapProvider.kt
@@ -17,35 +17,48 @@ class LdapProvider(private val ldapConfig: Map<*, *>, private val jwtConfig: Map
         System.setProperty("javax.net.ssl.keyStoreType", "Windows-my");
         System.setProperty("javax.net.ssl.trustStore", "NONE");
         System.setProperty("javax.net.ssl.trustStoreType", "Windows-ROOT");
-
+        // Check is the passwd is an env var
+        var bind_pass = ldapConfig["bind_pass"] as String?
+        if (bind_pass?.lowercase() == "env"){
+            bind_pass = System.getenv("LDAP_BIND_PASS")
+        }
         val env = Hashtable<String, String>()
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
         env.put(Context.SECURITY_AUTHENTICATION, "simple")
         env.put(Context.PROVIDER_URL, ldapConfig["address"] as String?);
         env.put(Context.SECURITY_PRINCIPAL, ldapConfig["bind_user"] as String?);
-        env.put(Context.SECURITY_CREDENTIALS, ldapConfig["bind_pass"] as String?);
+        env.put(Context.SECURITY_CREDENTIALS, bind_pass);
 
         try {
             ctx = InitialDirContext(env);
         } catch (ex: javax.naming.NamingException) {
             throw ex
         }
+    }
 
+    private fun getUserObject(username: String, password: String){
+        val searchControls = SearchControls()
+        searchControls.returningAttributes = arrayOf("cn");
+        searchControls.searchScope = SearchControls.SUBTREE_SCOPE
+
+        val ergebnis: NamingEnumeration<*>? =
+            ctx.search(
+                ldapConfig["base_dn"] as String?,
+                "(&(objectClass=person)(sAMAccountName=$username))",
+                searchControls
+            )
+    }
+
+    private fun getRoles(): List<String>{
+        return emptyList()
     }
 
     override fun login(username: String, password: String): String? {
         if(!this::ctx.isInitialized){
             connect()
         }
-        val ctls = SearchControls()
-        ctls.setSearchScope(SearchControls.SUBTREE_SCOPE)
 
-//        val ergebnis: NamingEnumeration<*>? =
-//            ctx.search(
-//                userDn,
-//                "(&(objectclass=user)(sAMAccountName=" + username + "))",
-//                ctls
-//            )
+
         return JWT.create()
             .withAudience(jwtConfig["audience"]as String?)
             .withIssuer(jwtConfig["issuer"]as String?)
