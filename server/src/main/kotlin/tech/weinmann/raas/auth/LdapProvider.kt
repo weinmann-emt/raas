5bc8d3c862c64fcca3cd5d6724348a3c851f281d
knkay <kay.richter@googlemail.com>
2025-08-21 15:56:52 +0200

    Auth working, creating token ok

--- server/src/main/kotlin/tech/weinmann/raas/auth/LdapProvider.kt
+++ server/src/main/kotlin/tech/weinmann/raas/auth/LdapProvider.kt
@@ -5,8 +5,13 @@ import io.ktor.server.auth.*
 import java.util.*
 import javax.naming.Context
 import javax.naming.NamingEnumeration
+import javax.naming.NamingException
 import javax.naming.directory.InitialDirContext
 import javax.naming.directory.SearchControls
+import javax.naming.directory.SearchResult
+import javax.naming.ldap.LdapContext
+
+
 
 
 class LdapProvider(private val ldapConfig: Map<*, *>, private val jwtConfig: Map<*, *>) : AuthProviderInterface {
@@ -31,38 +36,67 @@ class LdapProvider(private val ldapConfig: Map<*, *>, private val jwtConfig: Map
 
         try {
             ctx = InitialDirContext(env);
-        } catch (ex: javax.naming.NamingException) {
+        } catch (ex: NamingException) {
             throw ex
         }
     }
 
-    private fun getUserObject(username: String, password: String){
+    private fun getUserObject(username: String): String{
         val searchControls = SearchControls()
         searchControls.returningAttributes = arrayOf("cn");
         searchControls.searchScope = SearchControls.SUBTREE_SCOPE
 
-        val ergebnis: NamingEnumeration<*>? =
+        val result: NamingEnumeration<*>? =
             ctx.search(
                 ldapConfig["base_dn"] as String?,
                 "(&(objectClass=person)(sAMAccountName=$username))",
                 searchControls
             )
+        if (result == null){
+            throw NamingException("User not found")
+        }
+        val test = result.next() as SearchResult
+        return test.nameInNamespace
     }
 
+    //This can be adopted once we have more than simple manage of pi
     private fun getRoles(): List<String>{
-        return emptyList()
+        val ret = listOf("user")
+        return ret
+    }
+
+    public fun authenticate(dn: String, pass:String){
+        val env = Hashtable<String, String>()
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.SECURITY_AUTHENTICATION, "simple")
+        env.put(Context.PROVIDER_URL, ldapConfig["address"] as String?);
+        env.put(Context.SECURITY_PRINCIPAL, dn);
+        env.put(Context.SECURITY_CREDENTIALS, pass);
+
+        try {
+            ctx = InitialDirContext(env);
+        } catch (ex: NamingException) {
+            throw ex
+        }
     }
 
     override fun login(username: String, password: String): String? {
         if(!this::ctx.isInitialized){
             connect()
         }
-
+        val cn =getUserObject(username)
+        try {
+            authenticate(cn, password)
+        } catch (ex: NamingException) {
+            return null
+        }
+        val roles = getRoles()
 
         return JWT.create()
             .withAudience(jwtConfig["audience"]as String?)
             .withIssuer(jwtConfig["issuer"]as String?)
-            .withClaim("username", "")
+            .withClaim("username", username)
+            .withClaim("roles", roles)
             .withExpiresAt(Date(System.currentTimeMillis() + 60000))
             .sign(Algorithm.HMAC256(jwtConfig["secret"]as String?))
     }
