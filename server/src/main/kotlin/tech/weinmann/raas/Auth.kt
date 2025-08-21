f0b10101ca31fc7bba288afdfb466c7034bf5d41
kay <kay.richter@googlemail.com>
2025-08-21 22:05:57 +0200

    wip: super simple swagger thing working to validate token

--- server/src/main/kotlin/tech/weinmann/raas/Auth.kt
+++ server/src/main/kotlin/tech/weinmann/raas/Auth.kt
@@ -2,41 +2,42 @@ package tech.weinmann.raas
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.github.smiley4.ktoropenapi.get
 import io.github.smiley4.ktoropenapi.post
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.auth.Authentication
-import io.ktor.server.auth.basic
-import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.response.respond
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.koin.core.context.GlobalContext.get
-import org.koin.core.qualifier.named
-import org.koin.ktor.ext.get
 import org.koin.ktor.ext.inject
 import tech.weinmann.raas.auth.AuthProviderInterface
 import tech.weinmann.raas.auth.User
-import tech.weinmann.raas.configuration.RpiConfiguration
-import tech.weinmann.raas.devices.DeviceRepoInterface
 import kotlin.getValue
 
 fun Application.configureAuth(){
     val authService by inject<AuthProviderInterface>()
     install(Authentication) {
-        jwt("auth-jwt") {
+        jwt() {
             realm = authService.jwtConfig["realm"] as String
             verifier(
                 JWT
                 .require(Algorithm.HMAC256(authService.jwtConfig["secret"] as String))
-                .withAudience(authService.jwtConfig["realm"] as String)
-                .withIssuer(authService.jwtConfig["realm"] as String)
+                .withAudience(authService.jwtConfig["audience"] as String)
+                .withIssuer(authService.jwtConfig["issuer"] as String)
                 .build()
             )
-            challenge { defaultScheme, realm ->
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            challenge { _, _ ->
                 call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
             }
         }
@@ -65,10 +66,20 @@ fun Application.configureAuth(){
                 } else {
                     call.respond(HttpStatusCode.Unauthorized)
                 }
-
             }
-        }
+            authenticate {
+                get("/whoami", {
+                    description = "Some debug route"
+                }){
+                    val principal = call.principal<JWTPrincipal>()
+                    val username = principal!!.payload.getClaim("username").asString()
+                    val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                    call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+                }
+            }
         }
 
     }
+
+}
 
